9d9660cc38692ddbe24b4e8c5215cc9d4c3620a1
DragosZR <zarif.dragos@gmail.com>
2023-04-05 23:22:14 +0300

    DB conn + rectificari cod.

--- app.py
+++ app.py
@@ -1,43 +1,19 @@
 from flask import Flask, render_template, jsonify
+from database import load_jobs_from_db
 
 app = Flask(__name__)
 
-JOBS = [
-    {
-        'id': 1,
-        'title': 'Data Analyst',
-        'location': 'Cluj-Napoca, Romania',
-        'salary': '7420 RON',
-    },
-    {
-        'id': 2,
-        'title': 'Front-End Engineer',
-        'location': 'Cluj-Napoca, Romania',
-        'salary': '10100 RON'
-    },
-    {
-        'id': 3,
-        'title': 'Data Scientist',
-        'location': 'Cluj-Napoca, Romania',
-        'salary': '4400 RON'
-    },
-    {
-        'id': 4,
-        'title': 'Full-Stack Engineer',
-        'location': 'Cluj-Napoca, Romania',
-        'salary': '7100 RON'
-    }
-]
-
 
 @app.route("/")
-def hello_python():
-    return render_template('home.html', jobs=JOBS, company_name='TechLand')
+def home():
+    jobs = load_jobs_from_db()
+    return render_template('home.html', jobs=jobs, company_name='TechLand')
 
 
-@app.route("/jobs/")
+@app.route("/api/jobs/")
 def list_jobs():
-    return jsonify(JOBS)
+    jobs = load_jobs_from_db()
+    return jsonify(jobs)
 
 
 if __name__ == "__main__":
