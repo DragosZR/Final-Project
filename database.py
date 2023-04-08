6dd1813d2138cdaee5fb7d1931f6e6c6ae49cb6d
DragosZR <zarif.dragos@gmail.com>
2023-04-08 13:45:51 +0300

    Adaugare functionalitate de retinere a aplicatiilor in baza de date.

--- database.py
+++ database.py
@@ -24,9 +24,7 @@ def load_jobs_from_db():
 
 def load_job_from_db(id):
     with engine.connect() as conn:
-        result = conn.execute(
-            text(f"SELECT * FROM jobs WHERE id={id}")
-        )
+        result = conn.execute(text(f"SELECT * FROM jobs WHERE id={id}"))
         rows = []
         for row in result.all():
             rows.append(row._mapping)
@@ -34,3 +32,18 @@ def load_job_from_db(id):
             return None
         else:
             return row
+
+
+def add_application_to_db(job_id, data):
+    with engine.connect() as conn:
+        query = text(
+            "INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)")
+        values = {'job_id': job_id,
+                  'full_name': data['full_name'],
+                  'email': data['email'],
+                  'linkedin_url': data['linkedin_url'],
+                  'education': data['education'],
+                  'work_experience': data['work_experience'],
+                  'resume_url': data['resume_url']
+                  }
+        conn.execute(query, values)
